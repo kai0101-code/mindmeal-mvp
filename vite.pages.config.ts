7e3105eed7d8d300bc7ebf4e72d0866efc81baec
ChenYou Miao <chenyou7516@gmail.com>
2026-08-11 10:12:23 +0800

    Configure Pages for original repository

--- vite.pages.config.ts
+++ vite.pages.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/mindmeal_v2_backup/",
+  base: "/mindmeal-mvp/",
   build: {
     outDir: "dist-pages",
     emptyOutDir: true,
